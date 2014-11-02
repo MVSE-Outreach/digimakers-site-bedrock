6f4d814763fd1524643e5887518855f82bd1b507
Will Price <will.price94@gmail.com>
2014-11-02 10:35:27 +0000

    Add config changes

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -8,14 +8,14 @@ set :stage, :production
 
 # Extended Server Syntax
 # ======================
-server '192.168.50.5', user: 'vagrant', roles: %w{web app db}
+server 'digimakers.dev', user: 'deploy', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
   set :ssh_options, {
-    keys: ['~/.vagrant.d/insecure_private_key']
+    keys: ['~/.ssh/digimakers_do_rsa']
   }
 
 fetch(:default_env).merge!(wp_env: :production)
